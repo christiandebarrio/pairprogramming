89b15ed070b84c3f77cc03063fcf22b912ab7284
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-10-29 15:48:22 +0100

    stack dividing hours

--- srt.rb
+++ srt.rb
@@ -1,7 +1,14 @@
+require 'pry'
+
 class File
 
 	def initialize(file)
 		@file = file
+	end
+
+	def process_file
+		
+		div_time_in_out
 	end
 
 	def read_file
@@ -12,10 +19,27 @@ class File
 		read_file.split("\n\n")
 	end
 
-	def div_subtitle
-		div_text_in_subtitles.each do |subtitle|
+	def div_subtitle_in_elements
+		div_text_in_subtitles.map do |subtitle|
 			subtitle.split("\n")
 		end
+	end
+
+	def div_time_in_out
+		div_subtitle_in_elements.map do |subtitle|
+			subtitle.map do |element|
+				element[1] = element[1].split(" --> ")
+			end
+		end
+		# final = div_subtitle_in_elements.each_with_index do |subtitle, index|
+		# 	if index == 1
+		# 		subtitle[index] = subtitle[index].split(" --> ")
+		# 	else
+		# 		subtitle[index] = subtitle[index]
+		# 	end
+		# end
+		# return final
+
 	end
 
 	def writte_file
@@ -31,3 +55,9 @@ class Subtitle
 	end
 
 end
+
+ a = File.new("subtitles.srt")
+p a.process_file
+
+#binding.pry
+
