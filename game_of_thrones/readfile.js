7f76d5b168e70e2ff886efdbd03eb89674cdabe0
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-13 08:58:41 +0100

    episodes formated with star rating

--- game_of_thrones/readfile.js
+++ game_of_thrones/readfile.js
@@ -6,6 +6,16 @@ function fileActions(err, file){
     var episodes = JSON.parse(file);
     console.log("Title: " + episodes[0].title + "  Episode #:" + episodes[0].episode_number + "\r\n" +
       episodes[0].description + "\r\n" +
-      "Rating: " + episodes[0].rating + "\r\n");
+      "Rating: " + episodes[0].rating + " " + ratingPicture(episodes[0].rating) + "\r\n");
+
+    function ratingPicture(episode) {
+      var points = episode;
+      var ratingstar = "";
+      for(i = 1; i < points; i++) {
+        ratingstar += "*"
+      }
+      return ratingstar
+    }
+
 }
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
